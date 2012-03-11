a141a042c3dc04b84b7e470abf95bdcd5882f12d
Andrew Cholakian <andrew@andrewvc.com>
2012-03-11 16:08:28 -0700

    Initial import

---
+++ src/intro_project/core.clj
@@ -0,0 +1,1 @@
+(ns intro-project.core)
---
+++ test/intro_project/test/core.clj
@@ -0,0 +1,6 @@
+(ns intro-project.test.core
+  (:use [intro-project.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
